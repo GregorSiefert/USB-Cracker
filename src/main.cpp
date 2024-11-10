f08e5973d8653cee8e0ebc812d978875a5f1ba8f
Gregor Siefert <gregor.siefert@gmx.de>
2024-11-10 10:52:00 +0100

    Button Test

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,18 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const int pin_button1 = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  pinMode(pin_button1,INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (digitalRead(pin_button1))
+  {
+    Serial.write("pressed");
+    delay(100);
+  }
+  
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
